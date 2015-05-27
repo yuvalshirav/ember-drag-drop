ace191803c63d588128622af8c0dc9977ad29874
yuvalshirav <yuval@Yuvals-MacBook-Air.local>
2015-05-27 12:43:44 +0300

    send raw event on drop

--- addon/components/draggable-object-target.js
+++ addon/components/draggable-object-target.js
@@ -4,15 +4,15 @@ import Droppable from 'ember-drag-drop/mixins/droppable';
 export default Ember.Component.extend(Droppable, {
   classNames: ["draggable-object-target"],
 
-  handlePayload: function(payload) {
+  handlePayload: function(payload, event) {
     var obj = this.get('coordinator').getObject(payload,{target: this});
-    this.sendAction('action',obj,{target: this});
+    this.sendAction('action',obj,{target: this, event: event});
   },
 
   handleDrop: function(event) {
     var dataTransfer = event.dataTransfer;
     var payload = dataTransfer.getData("Text");
-    this.handlePayload(payload);
+    this.handlePayload(payload, event);
   },
 
   acceptDrop: function(event) {
